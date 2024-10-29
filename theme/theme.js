78cc8ac28b91384542731e8f64ace082c72313c8
touhidul-islam-html <ti360109@gmail.com>
2024-10-29 11:39:07 +0600

    theme button added

--- theme/theme.js
+++ theme/theme.js
@@ -1,27 +1,36 @@
+/* 
+    -to use the dark theme in a section just add the id name to the element "theme"
+    -the default dark theme style is given at the theme.css
+
+    -to add custom styles for theme to any section just folow this stracture:
+
+        .parentclass .dark-mode{
+            --add styles here--
+        }
+
+
+*/
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const toggle = document.getElementById('darkModeToggle');
-    const body = document.body;
+    const thmeitem = document.getElementById('theme');
 
     // Load user preference from localStorage
     const savedMode = localStorage.getItem('dark-mode') === 'true';
     if (savedMode) {
-        body.classList.add('dark-mode');
-        /* Select Elements and add dark-mode class to enable darkmode here ////////*/
-
+        thmeitem.classList.add('dark-mode');
         toggle.checked = true;
     }
 
     // Toggle dark mode
     toggle.addEventListener('change', () => {
         if (toggle.checked) {
-            body.classList.add('dark-mode');
-        /* Select Elements and add dark-mode class to enable darkmode here ///////////*/
-
+            thmeitem.classList.add('dark-mode');
             localStorage.setItem('dark-mode', 'true');
         } else {
-            body.classList.remove('dark-mode');
-        /* Select Elements and remove dark-mode class to disable darkmode here //////////*/
-
+            thmeitem.classList.remove('dark-mode');
             localStorage.setItem('dark-mode', 'false');
         }
     });
